8b82673f64089832f5043e13032d886a3915ce7f
Nestor Castillo <nes_23@yahoo.com>
2017-02-09 21:22:45 -0700

    add date picker component to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
 import BlankSlate from './BlankSlate';
-import { Link } from 'react-router';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
 import '../css/blankSlate.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment().startOf('week'),
+      endDate: moment().endOf('week')
+    }
+  }
+
+  handleChangeStart = (date) => {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  handleChangeEnd = (date) => {
+    this.setState({
+      endDate: date
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const { startDate, endDate } = this.state;
+
+    console.log(startDate.format());
+    console.log(endDate.format());
+    // dispatch(postMenu({startDate, endDate}));  
+  }
+
   render() {
     return (
       <BlankSlate 
         iconName='fa fa-calendar fa-3x' 
         title="Hey there! Looks like you don't have any menus as yet."
         meta='Select your first week.'>
-        <Link to='menus/new' className="empty-action btn btn-primary"> 
-          Start
-        </Link>
+        <form onSubmit={this.handleSubmit}>
+          <DatePicker
+            selected={this.state.startDate}
+            selectsStart  startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            onChange={this.handleChangeStart} />
+          <DatePicker
+            selected={this.state.endDate}
+            selectsEnd  startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            onChange={this.handleChangeEnd} />
+          <button type="submit" className="btn btn-primary btn-sm">Start</button>
+        </form>
+        
       </BlankSlate>
     )
   }
