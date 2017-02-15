4b4ce5e2cf8c6dc82a84bee1c3f5bb31d8cefbff
Nestor Castillo <nes_23@yahoo.com>
2017-02-14 21:50:16 -0700

    WIP: Adding tabs components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class Menu extends Component {
   render() {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     return (
-      <div>
-        <ul className="tab tab-block">
-          <li className="tab-item active">
-            <a href="#">Music</a>
-          </li>
-          <li className="tab-item">
-            <a href="#" >Playlists</a>
-          </li>
-          <li className="tab-item">
-            <a href="#">Radio</a>
-          </li>
-          <li className="tab-item">
-            <a href="#">Connect</a>
-          </li>
-        </ul>
-      </div>
+      <Tabs>
+        <TabList>
+          {days.map ((day, index) => <Tab key={index}>{day}</Tab>)}
+        </TabList>
+        <TabPanel>
+          <h2>Sunday Meal List</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Monday</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Tuesday</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Wednesday</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Thursday</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Friday</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Saturday</h2>
+        </TabPanel>
+      </Tabs>
     )
   }
 }
